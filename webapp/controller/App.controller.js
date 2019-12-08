2ccdb98e7d477bb74cd82ed3122dfb785eac8aec
Gustavo Fontinha <gustavofd@gmail.com>
2019-12-08 12:39:58 +0000

    Última versão.

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -5,16 +5,16 @@ sap.ui.define(
 		"sap/ui/model/Filter",
 		"sap/ui/model/FilterOperator",
 		"opensap/myapp/model/formatter"
-	]
-	,function(Controller, MessageToast, Filter, FilterOperator, formatter){
-		
-		Controller.extend("opensap.myapp.controller.App",{
+	],
+	function (Controller, MessageToast, Filter, FilterOperator, formatter) {
 
-		    formatter: formatter,
+		Controller.extend("opensap.myapp.controller.App", {
 
-			onShowHello: function(){
+			formatter: formatter,
+
+			onShowHello: function () {
 				//MessageToast.show("Que coisa mais linda!");
-				
+
 				// read msg from i18n model
 				var oBundle = this.getView().getModel("i18n").getResourceBundle();
 				var sRecipient = this.getView().getModel("helloPanel").getProperty("/recipient/name");
@@ -24,23 +24,26 @@ sap.ui.define(
 				MessageToast.show(sMsg);
 			},
 
-			onFilterProducts : function (oEvent) {
-			    var aFilter = [] , sQuery = oEvent.getParameter("query"),
-			    oList = this.getView().byId("productsList"),
-			    oBinding = oList.getBinding("items");
-			    if (sQuery){
-			        aFilter.push(new Filter("ProductID", FilterOperator.Contains,sQuery));
-			    }
-			    oBinding.filter(aFilter);
+			onFilterProducts: function (oEvent) {
+				var aFilter = [],
+					sQuery = oEvent.getParameter("query"),
+					oList = this.getView().byId("productsList"),
+					oBinding = oList.getBinding("items");
+				if (sQuery) {
+					aFilter.push(new Filter("ProductID", FilterOperator.Contains, sQuery));
+				}
+				oBinding.filter(aFilter);
 			},
 
-			onItemSelected: function(oEvent) {
+			onItemSelected: function (oEvent) {
 				var oSelectedItem = oEvent.getSource();
 				var oContext = oSelectedItem.getBindingContext();
 				var sPath = oContext.getPath();
-				var oProductDetailPanel = this.byId("productDatailsPanel");
+				var oProductDetailPanel = this.byId("productDetailsPanel");
 
-				oProductDetailPanel.bindElement({path: sPath});
+				oProductDetailPanel.bindElement({
+					path: sPath
+				});
 				this.byId("productDetailsPanel").setVisible(true);
 			}
 
